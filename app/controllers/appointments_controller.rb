1a0b3844077ffb64c2047dc8d0034a6b44e0b2c3
CH-JesseMa <jesse@casthaven.com>
2014-05-12 17:15:02 -0400

    set up controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,57 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.all
   end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+     @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to @appointment
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(params[:appointment].permit(
+      :appointment_time,
+      :client_email,
+      :client_name,
+      :client_mobile,
+      :appointment_duration,
+      :appointment_location))
+    end
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to appointments_path
+  end
+
 end
+
+private
+
+  def appointment_params
+    params.require(:appointment).permit(
+      :appointment_time,
+      :client_email,
+      :client_name,
+      :client_mobile,
+      :appointment_duration,
+      :appointment_location)
+  end
