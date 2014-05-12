69c0fea2a7ccebeb071a488f5c44f485e4c499ce
CH-JesseMa <jesse@casthaven.com>
2014-05-12 16:43:09 -0400

    add appts and articles controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'articles/index'
+
+  get 'appointments/index'
+
   devise_for :users, :path => 'accounts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
