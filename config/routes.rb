ca62511cf83ad0043d5915c7585cb01cd063b8ad
CH-JesseMa <jesse@casthaven.com>
2014-05-12 16:36:35 -0400

    nested appointments/articles to user, changed path for devise to accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :path => 'accounts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  resources :users do
+    resources :articles
+    resources :appointments
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
